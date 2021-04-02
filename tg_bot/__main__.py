8e037e3d5fd52daec7a1c3c7ea2fd34df7d9f9ff
darkcobra584 <80756166+darkcobra584@users.noreply.github.com>
2021-04-03 00:15:09 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -143,7 +143,8 @@ def start(bot: Bot, update: Update, args: List[str]):
                                                                        callback_data="help_back".format(bot.username)),
                                                                                    InlineKeyboardButton(text="🧑‍💻My Creator🧑‍💻",
                                                                        url="t.me/ANlME_SENPAI")],
-                                                                                   [InlineKeyboardButton(text=MIZUHARA TO YOUR GROUP",
+                                                                                   [InlineKeyboardButton(text= 
+"MIZUHARA TO YOUR GROUP",
                                                                        url="t.me/{}?startgroup=true".format(bot.username)),
                                                                                    InlineKeyboardButton(text="Source Code",
                                                                        url="https://github.com/legendx22/GRANDROBOT")
